d73d03502e4bef4b074fd0c50425fd75929e7067
khangtran <kt36@njit.edu>
2020-12-03 21:45:10 -0500

    Task: add app.py and templates

---
+++ app/app.py
@@ -0,0 +1,49 @@
+from typing import List, Dict
+import simplejson as json
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask import render_template
+from flaskext.mysql import MySQL
+from pymysql.cursors import DictCursor
+import hashlib
+
+app = Flask(__name__)
+mysql = MySQL(cursorclass=DictCursor)
+
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+app.config['MYSQL_DATABASE_DB'] = 'userData'
+mysql.init_app(app)
+message = ''
+
+@app.route('/', methods=['GET'])
+def index(ms=message):
+    return render_template('homepage.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    message=''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = hashlib.md5(request.form['password'].encode(encoding='UTF-8',errors='strict')).hexdigest()
+        cursor = mysql.get_db().cursor()
+        cursor.execute('SELECT * FROM userTable WHERE username = %s AND password = %s', (username, password,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+        if account:
+            # Create session data, we can access this data in other routes
+            session['id'] = account['id']
+            session['username'] = account['username']
+            # Redirect to home page
+            message = 'Logged in successfully!'
+            return render_template('layout.html', msg=message)
+        else:
+            # Account doesnt exist or username/password incorrect
+            message = 'Incorrect username/password!'
+    return render_template('login.html', msg=message)
+
+if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.run(host='0.0.0.0', debug=True)
