bf3e5e6b187213112791eb6a67b6c6786a54c783
khangtran <kt36@njit.edu>
2020-12-08 20:30:19 -0500

    Feature: Register without verification

--- app/app.py
+++ app/app.py
@@ -9,11 +9,17 @@ import hashlib
 app = Flask(__name__)
 mysql = MySQL(cursorclass=DictCursor)
 
-app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-east-02.cleardb.com'
-app.config['MYSQL_DATABASE_USER'] = 'b8245d5e04777e'
-app.config['MYSQL_DATABASE_PASSWORD'] = '1027a2d6'
+# app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-east-02.cleardb.com'
+# app.config['MYSQL_DATABASE_USER'] = 'b8245d5e04777e'
+# app.config['MYSQL_DATABASE_PASSWORD'] = '1027a2d6'
+# app.config['MYSQL_DATABASE_PORT'] = 3306
+# app.config['MYSQL_DATABASE_DB'] = 'heroku_a6d0e70501b6383'
+
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_DB'] = 'heroku_a6d0e70501b6383'
+app.config['MYSQL_DATABASE_DB'] = 'userData'
 mysql.init_app(app)
 message = ''
 
@@ -36,13 +42,12 @@ def login():
         account = cursor.fetchone()
         if account:
             # Create session data, we can access this data in other routes
-            # session['id'] = account['id']
-            # session['username'] = account['username']
-            # Redirect to home page
+            session['id'] = account['id']
+            session['username'] = account['username']
             message = 'Logged in successfully!'
-
             return render_template('profile.html', firstname=account['firstname'], lastname=account['lastname'],
-                                   school=account['department'], year=account['year'])
+                                   school=account['school'], department=account['department'], year=account['year'],
+                                   activate=account['isactivate'])
         else:
             # Account doesnt exist or username/password incorrect
             message = 'Incorrect username/password!'
@@ -51,11 +56,45 @@ def login():
 
 @app.route('/logout', methods=['GET'])
 def logout():
-    # session.pop('username', None)
-    # session.pop('id', None)
+    session.pop('username', None)
+    session.pop('id', None)
     return redirect('/', code=302)
+
+
+@app.route('/register', methods=['GET'])
+def register_get():
+    message = ''
+    return render_template('register.html', msg=message)
+
+
+@app.route('/register', methods=['POST'])
+def register_post():
+    message = ''
+    cursor = mysql.get_db().cursor()
+    username = request.form['username']
+    cursor.execute('SELECT * FROM userTable WHERE username = %s', (username))
+    account = cursor.fetchone()
+    if account:
+        message = 'username already exist'
+        return render_template('register.html', msg=message)
+    if request.form['password'] != request.form['confirm_password']:
+        message = 'password is not match'
+        return render_template('register.html', msg=message)
+    inputData = (request.form.get('username'),
+                 hashlib.md5(request.form['password'].encode(encoding='UTF-8', errors='strict')).hexdigest(),
+                 request.form.get('firstname'),
+                 request.form.get('lastname'), request.form.get('school'),
+                 request.form.get('department'), request.form.get('year'), False)
+    sql_insert_query = """INSERT INTO userTable (username,password,firstname,lastname,school,department,`year`,isactivate) VALUES (%s, %s,%s, %s,%s, %s,%s,%s) """
+    cursor.execute(sql_insert_query, inputData)
+    mysql.get_db().commit()
+    return render_template('profile.html', firstname=request.form.get('firstname'),
+                           lastname=request.form.get('lastname'),
+                           school=request.form.get('school'), department=request.form.get('department'),
+                           year=request.form.get('year'),
+                           activate=request.form.get('isactivate'))
 
 
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
-    app.run()
+    app.run(host='0.0.0.0')
